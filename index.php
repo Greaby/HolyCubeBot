3c92ef6929025442621399ca6b47181d3a772056
Greaby <hello@greaby.co>
2021-11-27 08:35:05 +0100

    update API link and auto delete live tweets

--- index.php
+++ index.php
@@ -9,12 +9,24 @@ use DG\Twitter\Twitter;
 use Symfony\Component\Dotenv\Dotenv;
 
 $dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/.env');
+$dotenv->load(__DIR__ . '/.env');
 
 
-function getPlayer($list, $id) {
+$twitterAPI = new Twitter(
+    $_ENV["TWITTER_CONSUMER_KEY"],
+    $_ENV["TWITTER_CONSUMER_SECRET"],
+    $_ENV["TWITTER_ACCESS_TOKEN_KEY"],
+    $_ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+);
+
+
+deleteOldTweet($twitterAPI);
+
+
+function getPlayer($list, $id)
+{
     foreach ($list as $player) {
-        if($player["@id"] === $id) {
+        if ($player["@id"] === $id) {
             return $player;
         }
     }
@@ -23,24 +35,38 @@ function getPlayer($list, $id) {
 }
 
 
-function sendTwitter($message) {
+function deleteOldTweet($twitterAPI)
+{
+    $tweets = $twitterAPI->load(Twitter::ME);
+
+    foreach ($tweets as $tweet) {
+
+        if (strpos($tweet->text, 'En live') === 0) {
+
+
+            if (time() - strtotime($tweet->created_at) > 43200) { # greater than 12 hours
+                if ($_ENV["ENVIRONMENT"] === "production") {
+                    $twitterAPI->destroy($tweet->id);
+                }
+                echo "deleted tweet " . $tweet->id . "<br>";
+            }
+        }
+    }
+}
+
+
+
+function sendTwitter($twitterAPI, $message)
+{
     try {
         echo $message . "<br>";
 
-        if($_ENV["ENVIRONMENT"] === "production") {
-            $twitter = new Twitter(
-                $_ENV["TWITTER_CONSUMER_KEY"], 
-                $_ENV["TWITTER_CONSUMER_SECRET"], 
-                $_ENV["TWITTER_ACCESS_TOKEN_KEY"], 
-                $_ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-            );
-            $twitter->send($message);
+        if ($_ENV["ENVIRONMENT"] === "production") {
+            $twitterAPI->send($message);
         }
-        
     } catch (exception $e) {
         echo $e->getMessage();
     }
-   
 }
 
 
@@ -52,44 +78,44 @@ $filename_videos = "data/" . $_ENV["FILENAME_VIDEOS"];
 $last_players = null;
 $last_videos = null;
 
-if(file_exists($filename_players)) {
+if (file_exists($filename_players)) {
     $last_players = json_decode(file_get_contents($filename_players), true);
 }
 
-if(file_exists($filename_videos)) {
+if (file_exists($filename_videos)) {
     $last_videos = json_decode(file_get_contents($filename_videos), true);
 }
 
 
 // Fetch API
-$players = json_decode(file_get_contents("https://api.holycube.fr/players"), true);
-$videos = json_decode(file_get_contents("https://api.holycube.fr/videos"), true);
+$players = json_decode(file_get_contents("https://www.holycube.fr/players"), true);
+$videos = json_decode(file_get_contents("https://www.holycube.fr/videos"), true);
 
 
 // send last videos
-if($last_videos !== null) {
+if ($last_videos !== null) {
 
-    $ids = array_map(function($video) {
+    $ids = array_map(function ($video) {
         return $video["videoId"];
     }, $last_videos["hydra:member"]);
 
 
     foreach ($videos["hydra:member"] as $video) {
-        if(!in_array($video["videoId"], $ids)) {
+        if (!in_array($video["videoId"], $ids)) {
             $player = getPlayer($players["hydra:member"], $video["player"]);
-            sendTwitter(join(" ", ["Nouvelle vidéo de", "@".$player["twitterName"], $video["title"], "https://www.youtube.com/watch?v=".$video["videoId"], "#HolyCube", "#Minecraft"]));
+            sendTwitter($twitterAPI, join(" ", ["Vidéo de", "@" . $player["twitterName"], $video["title"], "https://www.youtube.com/watch?v=" . $video["videoId"], "#HolyCube", "#Minecraft"]));
         }
     }
 }
 
 
 // send player is live
-if($last_players !== null) {
+if ($last_players !== null) {
     foreach ($players["hydra:member"] as $player) {
         $last_players_data = getPlayer($last_players["hydra:member"], $player["@id"]);
-        
-        if(!empty($last_players_data) and !empty($player["twitchName"]) and $player["isLiveHolycube"] and !$last_players_data["isLiveHolycube"]) {
-            sendTwitter(join(" ", ["En live", "@".$player["twitterName"], $player["liveName"], "https://www.twitch.tv/". $player["twitchName"], "#HolyCube", "#Minecraft"]));
+
+        if (!empty($last_players_data) and !empty($player["twitchName"]) and $player["isLiveHolycube"] and !$last_players_data["isLiveHolycube"]) {
+            sendTwitter($twitterAPI, join(" ", ["En live 🔴", "@" . $player["twitterName"], $player["liveName"], "https://www.twitch.tv/" . $player["twitchName"], "#HolyCube", "#Minecraft"]));
         }
     }
 }
